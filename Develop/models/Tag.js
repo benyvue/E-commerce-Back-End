ed8e79ef78b015619e13eb875c321dc45c35e158
MightyBenno <88509973+benyvue@users.noreply.github.com>
2021-11-21 13:57:24 -0600

    finished models

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -6,7 +6,17 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // define id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define tag_name column
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
@@ -16,5 +26,6 @@ Tag.init(
     modelName: 'tag',
   }
 );
+
 
 module.exports = Tag;
